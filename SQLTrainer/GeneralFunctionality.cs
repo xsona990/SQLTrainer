176416bd36d17e78185b528186bfd89f481a2cfd
Kostenko Ihor <xsona990@gmail.com>
2017-07-14 15:02:46 +0300

    Minor improvements

--- SQLTrainer/GeneralFunctionality.cs
+++ SQLTrainer/GeneralFunctionality.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SQLite;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SQLTrainer
 {
